86fa93a6bcf8dbdcf59fec816fa738a545ce6e93
Tom Eklöf <tome@unity3d.com>
2014-10-18 14:03:07 +0300

    add functional tests for UnsafeWriter and SyncWriter

--- functional_test.go
+++ functional_test.go
@@ -42,6 +42,111 @@ func checkKafkaAvailability(t *testing.T) {
 	}
 }
 
+func TestUnsafeWriterFunctional(t *testing.T) {
+
+	checkKafkaAvailability(t)
+
+	client, err := NewClient("functional_test", []string{kafkaAddr}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+
+	consumerConfig := NewConsumerConfig()
+	consumerConfig.OffsetMethod = OffsetMethodNewest
+
+	consumer, err := NewConsumer(client, "single_partition", 0, "functional_test", consumerConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer consumer.Close()
+	pc := NewProducerConfig()
+	pc.MaxBufferTime = 100 * time.Millisecond
+	producer, err := client.NewUnsafeWriter("single_partition", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var msg []byte
+	for i := 1; i <= TestBatchSize; i++ {
+		msg = []byte(fmt.Sprintf("testing %d", i))
+		n, err := producer.Write(msg)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if n != len(msg) {
+			t.Fatal("Wrote", n, "bytes, expected", len(msg))
+		}
+	}
+	producer.Close()
+
+	events := consumer.Events()
+	for i := 1; i <= TestBatchSize; i++ {
+		select {
+		case <-time.After(10 * time.Second):
+			t.Fatal("Not received any more events in the last 10 seconds.")
+
+		case event := <-events:
+			if string(event.Value) != fmt.Sprintf("testing %d", i) {
+				t.Fatalf("Unexpected message with index %d: %s", i, event.Value)
+			}
+		}
+
+	}
+
+}
+
+func TestSyncWriterFunctional(t *testing.T) {
+
+	checkKafkaAvailability(t)
+
+	client, err := NewClient("functional_test", []string{kafkaAddr}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+
+	consumerConfig := NewConsumerConfig()
+	consumerConfig.OffsetMethod = OffsetMethodNewest
+
+	consumer, err := NewConsumer(client, "single_partition", 0, "functional_test", consumerConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer consumer.Close()
+
+	producer, err := client.NewSyncWriter("single_partition", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var msg []byte
+	for i := 1; i <= TestBatchSize; i++ {
+		msg = []byte(fmt.Sprintf("testing %d", i))
+		n, err := producer.Write(msg)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if n != len(msg) {
+			t.Fatal("Wrote", n, "bytes, expected", len(msg))
+		}
+	}
+	producer.Close()
+
+	events := consumer.Events()
+	for i := 1; i <= TestBatchSize; i++ {
+		select {
+		case <-time.After(10 * time.Second):
+			t.Fatal("Not received any more events in the last 10 seconds.")
+
+		case event := <-events:
+			if string(event.Value) != fmt.Sprintf("testing %d", i) {
+				t.Fatalf("Unexpected message with index %d: %s", i, event.Value)
+			}
+		}
+
+	}
+
+}
+
 func TestProducingMessages(t *testing.T) {
 	checkKafkaAvailability(t)
 
