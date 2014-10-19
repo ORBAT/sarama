5b9c57aeececb44bd295fdea75db66ee63aebdd8
Tom Eklöf <tome@unity3d.com>
2014-10-19 15:39:58 +0300

    comments, set ChannelBufferSize when creating a new QueuingWriter from a client

--- queuing_writer.go
+++ queuing_writer.go
@@ -10,7 +10,7 @@ import (
 )
 
 // QueuingWriter is an io.Writer that writes messages to Kafka. Parallel calls to Write() will cause messages to be queued by the producer.
-// Each Write() call will block until a response is received.
+// Each Write() call will block until a response is received. The number of concurrent writes is limited by ChannelBufferSize
 type QueuingWriter struct {
 	kp       *Producer
 	id       string
@@ -19,24 +19,26 @@ type QueuingWriter struct {
 	log      *log.Logger
 	// if CloseClient is true, the client will be closed when Close() is called, effectively turning Close() into CloseBoth()
 	CloseClient bool
-	latestMsg   *ProduceError
-	// mutex for latestMsg
+
+	// latestMsg is the latest response received from the broker
+	latestMsg *ProduceError
+	// latestMut is the mutex for latestMsg
 	latestMut sync.Mutex
-	recvCond  *sync.Cond
+	// recvCond is the condition Write() waits on
+	recvCond *sync.Cond
 }
 
 func (kp *Producer) NewQueuingWriter(topic string) (p *QueuingWriter, err error) {
 	if !kp.config.AckSuccesses {
 		return nil, errors.New("AckSuccesses was false")
 	}
-
 	id := "queuingw-" + TimestampRandom()
 	pl := NewLogger(fmt.Sprintf("QueuingWr %s -> %s", id, topic), nil)
 	p = &QueuingWriter{kp: kp, id: id, topic: topic, log: pl, closedCh: make(chan struct{})}
 	p.recvCond = sync.NewCond(&p.latestMut)
 
-	go func(p *QueuingWriter, errCh <-chan *ProduceError) {
-		// errCh := kp.Errors()
+	go func(p *QueuingWriter) {
+		errCh := p.kp.Errors()
 		p.log.Println("Starting response listener")
 		for {
 			select {
@@ -61,7 +63,7 @@ func (kp *Producer) NewQueuingWriter(topic string) (p *QueuingWriter, err error)
 				runtime.Gosched()
 			}
 		}
-	}(p, kp.Errors())
+	}(p)
 
 	return
 }
@@ -72,8 +74,8 @@ func (c *Client) NewQueuingWriter(topic string, config *ProducerConfig) (p *Queu
 		config = NewProducerConfig()
 	}
 
+	config.ChannelBufferSize = 10
 	config.AckSuccesses = true
-	config.FlushMsgCount = 5
 
 	kp, err := NewProducer(c, config)
 	if err != nil {
@@ -99,8 +101,12 @@ func (qw *QueuingWriter) Write(p []byte) (n int, err error) {
 	n = len(p)
 	// counter := <-nums
 	msg := &MessageToSend{Topic: qw.topic, Key: nil, Value: ByteEncoder(p)}
+
 	qw.kp.Input() <- msg
+
 	qw.latestMut.Lock()
+
+	// wait until latestMsg contains the message we sent
 	for qw.latestMsg == nil || qw.latestMsg.Msg != msg {
 		qw.recvCond.Wait()
 	}
