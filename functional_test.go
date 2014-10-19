7f24c46b22f2bd4e2d0f08a454fe6ae30f58bce6
Tom Eklöf <tome@unity3d.com>
2014-10-19 19:44:42 +0300

    move writer tests to bottom for saner merging

--- functional_test.go
+++ functional_test.go
@@ -45,6 +45,99 @@ func checkKafkaAvailability(t *testing.T) {
 	}
 }
 
+func TestFuncProducing(t *testing.T) {
+	config := NewProducerConfig()
+	testProducingMessages(t, config)
+}
+
+func TestFuncProducingGzip(t *testing.T) {
+	config := NewProducerConfig()
+	config.Compression = CompressionGZIP
+	testProducingMessages(t, config)
+}
+
+func TestFuncProducingSnappy(t *testing.T) {
+	config := NewProducerConfig()
+	config.Compression = CompressionSnappy
+	testProducingMessages(t, config)
+}
+
+func TestFuncProducingNoResponse(t *testing.T) {
+	config := NewProducerConfig()
+	config.RequiredAcks = NoResponse
+	testProducingMessages(t, config)
+}
+
+func TestFuncProducingFlushing(t *testing.T) {
+	config := NewProducerConfig()
+	config.FlushMsgCount = TestBatchSize / 8
+	config.FlushFrequency = 250 * time.Millisecond
+	testProducingMessages(t, config)
+}
+
+func testProducingMessages(t *testing.T, config *ProducerConfig) {
+	checkKafkaAvailability(t)
+
+	client, err := NewClient("functional_test", []string{kafkaAddr}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+
+	consumerConfig := NewConsumerConfig()
+	consumerConfig.OffsetMethod = OffsetMethodNewest
+
+	consumer, err := NewConsumer(client, "single_partition", 0, "functional_test", consumerConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer consumer.Close()
+
+	config.AckSuccesses = true
+	producer, err := NewProducer(client, config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedResponses := TestBatchSize
+	for i := 1; i <= TestBatchSize; {
+		msg := &MessageToSend{Topic: "single_partition", Key: nil, Value: StringEncoder(fmt.Sprintf("testing %d", i))}
+		select {
+		case producer.Input() <- msg:
+			i++
+		case ret := <-producer.Errors():
+			if ret.Err == nil {
+				expectedResponses--
+			} else {
+				t.Fatal(ret.Err)
+			}
+		}
+	}
+	for expectedResponses > 0 {
+		ret := <-producer.Errors()
+		if ret.Err == nil {
+			expectedResponses--
+		} else {
+			t.Fatal(ret.Err)
+		}
+	}
+	producer.Close()
+
+	events := consumer.Events()
+	for i := 1; i <= TestBatchSize; i++ {
+		select {
+		case <-time.After(10 * time.Second):
+			t.Fatal("Not received any more events in the last 10 seconds.")
+
+		case event := <-events:
+			if string(event.Value) != fmt.Sprintf("testing %d", i) {
+				t.Fatalf("Unexpected message with index %d: %s", i, event.Value)
+			}
+		}
+
+	}
+}
+
 func newParallelProdConf() *ProducerConfig {
 	pc := NewProducerConfig()
 	pc.FlushFrequency = 50 * time.Millisecond
@@ -251,97 +344,4 @@ func TestFuncSyncWriter(t *testing.T) {
 		t.Fatal(err)
 	}
 	testWriter(client, w, t)
-}
-
-func TestFuncProducing(t *testing.T) {
-	config := NewProducerConfig()
-	testProducingMessages(t, config)
-}
-
-func TestFuncProducingGzip(t *testing.T) {
-	config := NewProducerConfig()
-	config.Compression = CompressionGZIP
-	testProducingMessages(t, config)
-}
-
-func TestFuncProducingSnappy(t *testing.T) {
-	config := NewProducerConfig()
-	config.Compression = CompressionSnappy
-	testProducingMessages(t, config)
-}
-
-func TestFuncProducingNoResponse(t *testing.T) {
-	config := NewProducerConfig()
-	config.RequiredAcks = NoResponse
-	testProducingMessages(t, config)
-}
-
-func TestFuncProducingFlushing(t *testing.T) {
-	config := NewProducerConfig()
-	config.FlushMsgCount = TestBatchSize / 8
-	config.FlushFrequency = 250 * time.Millisecond
-	testProducingMessages(t, config)
-}
-
-func testProducingMessages(t *testing.T, config *ProducerConfig) {
-	checkKafkaAvailability(t)
-
-	client, err := NewClient("functional_test", []string{kafkaAddr}, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer client.Close()
-
-	consumerConfig := NewConsumerConfig()
-	consumerConfig.OffsetMethod = OffsetMethodNewest
-
-	consumer, err := NewConsumer(client, "single_partition", 0, "functional_test", consumerConfig)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer consumer.Close()
-
-	config.AckSuccesses = true
-	producer, err := NewProducer(client, config)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expectedResponses := TestBatchSize
-	for i := 1; i <= TestBatchSize; {
-		msg := &MessageToSend{Topic: "single_partition", Key: nil, Value: StringEncoder(fmt.Sprintf("testing %d", i))}
-		select {
-		case producer.Input() <- msg:
-			i++
-		case ret := <-producer.Errors():
-			if ret.Err == nil {
-				expectedResponses--
-			} else {
-				t.Fatal(ret.Err)
-			}
-		}
-	}
-	for expectedResponses > 0 {
-		ret := <-producer.Errors()
-		if ret.Err == nil {
-			expectedResponses--
-		} else {
-			t.Fatal(ret.Err)
-		}
-	}
-	producer.Close()
-
-	events := consumer.Events()
-	for i := 1; i <= TestBatchSize; i++ {
-		select {
-		case <-time.After(10 * time.Second):
-			t.Fatal("Not received any more events in the last 10 seconds.")
-
-		case event := <-events:
-			if string(event.Value) != fmt.Sprintf("testing %d", i) {
-				t.Fatalf("Unexpected message with index %d: %s", i, event.Value)
-			}
-		}
-
-	}
 }
