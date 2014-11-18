6e01a43a2a0b481e606c22689470c7608d48c575
Tom Eklöf <tome@unity3d.com>
2014-11-18 17:59:13 +0200

    use "promise"-based Producer API in *Writer

--- queuing_writer.go
+++ queuing_writer.go
@@ -10,101 +10,38 @@ import (
 	"syscall"
 )
 
-type work struct {
-	resultCh chan error // channel to return result of write on
-	*MessageToSend
-}
-
 // QueuingWriter is an io.Writer that writes messages to Kafka. Parallel calls to Write() will cause messages to be queued by the producer, and
 // each Write() call will block until a response is received.
 type QueuingWriter struct {
 	kp          *Producer
 	id          string
 	topic       string
-	stopCh      chan struct{}
 	closed      int32 // nonzero if the writer has been closed
 	log         *log.Logger
-	CloseClient bool         // if CloseClient is true, the client will be closed when Close() is called, effectively turning Close() into CloseAll()
-	mut         sync.RWMutex // mutex for errChForMsg
-	closeMut    sync.Mutex   // mutex for Close and CloseAll
-	errChForMsg map[*MessageToSend]chan error
+	CloseClient bool           // if CloseClient is true, the client will be closed when Close() is called, effectively turning Close() into CloseAll()
+	closeMut    sync.Mutex     // mutex for Close and CloseAll
 	pendingWg   sync.WaitGroup // WaitGroup for pending message responses
-	workCh      chan work      // channel for sending writes to event loop
 }
 
-// DefaultChanBufferSize is the default ChannelBufferSize to use when NewQueuingWriter is called with a nil config.
-var DefaultChanBufferSize = 20
-
-// NewQueuingWriter creates a new Producer for the client and returns a QueuingWriter for that Producer. If a nil *ProducerConfig is given, default configuration will be used
-// and ChannelBufferSize will be set to DefaultChanBufferSize.
+// NewQueuingWriter creates a new Producer for the client and returns a QueuingWriter for that Producer. If a nil *ProducerConfig is given, NewProducerConfig() will be used.
 func (c *Client) NewQueuingWriter(topic string, config *ProducerConfig) (p *QueuingWriter, err error) {
 
 	if config == nil {
 		config = NewProducerConfig()
-		config.ChannelBufferSize = DefaultChanBufferSize
 	}
-
-	config.AckSuccesses = true
 
 	kp, err := NewProducer(c, config)
 	if err != nil {
 		return nil, err
 	}
+
 	id := "qw-" + TimestampRandom()
 	pl := NewLogger(fmt.Sprintf("QueuingWr %s -> %s", id, topic), nil)
 	p = &QueuingWriter{kp: kp,
-		id:          id,
-		topic:       topic,
-		log:         pl,
-		stopCh:      make(chan struct{}),
-		errChForMsg: make(map[*MessageToSend]chan error),
-		workCh:      make(chan work, config.ChannelBufferSize)}
-
-	go func() {
-		errCh := p.kp.Errors()
-		succCh := p.kp.Successes()
-
-		p.log.Println("Starting event loop")
-		for {
-			select {
-			case <-p.stopCh:
-				p.log.Println("Stopping event loop")
-				return
-			case work := <-p.workCh:
-				p.errChForMsg[work.MessageToSend] = work.resultCh
-				go func() {
-					p.kp.Input() <- work.MessageToSend
-				}()
-			case perr, ok := <-errCh:
-				if !ok {
-					p.log.Println("Errors() channel closed?!")
-					close(p.stopCh)
-					continue
-				}
-				p.sendProdResponse(perr.Msg, perr.Err)
-			case succ, ok := <-succCh:
-				if !ok {
-					p.log.Println("Successes() channel closed?!")
-					close(p.stopCh)
-					continue
-				}
-				// p.log.Printf("Received %p", succ)
-				p.sendProdResponse(succ, nil)
-			}
-		}
-	}()
-
+		id:    id,
+		topic: topic,
+		log:   pl}
 	return
-}
-
-func (qw *QueuingWriter) sendProdResponse(msg *MessageToSend, perr error) {
-	receiver, ok := qw.errChForMsg[msg]
-	if !ok {
-		qw.log.Panicf("Nobody wanted *MessageToSend %p (%#v)? Map is %#v", msg, msg, qw.errChForMsg)
-	} else {
-		receiver <- perr
-		delete(qw.errChForMsg, msg)
-	}
 }
 
 // Write will queue p as a single message, blocking until a response is received.
@@ -116,12 +53,18 @@ func (qw *QueuingWriter) sendProdResponse(msg *MessageToSend, perr error) {
 func (qw *QueuingWriter) Write(p []byte) (n int, err error) {
 	n = len(p)
 	resCh := make(chan error, 1)
-	wrk := work{MessageToSend: &MessageToSend{Topic: qw.topic, Key: nil, Value: ByteEncoder(p)}, resultCh: resCh}
+
+	msg := &MessageToSend{Topic: qw.topic, Key: nil, Value: ByteEncoder(p), Promise: resCh}
+
 	qw.pendingWg.Add(1)
 	defer qw.pendingWg.Done()
-	qw.workCh <- wrk
+	qw.kp.Input() <- msg
 
 	select {
+	case perr := <-qw.kp.Errors(): // note: temporary until Producer API is frozen
+		close(resCh) // might cause runtime to hurl with send on closed channel but oh well
+		err = perr.Err
+		n = 0
 	case resp := <-resCh:
 		close(resCh)
 		if resp != nil {
@@ -134,7 +77,7 @@ func (qw *QueuingWriter) Write(p []byte) (n int, err error) {
 }
 
 // ReadFrom reads all available bytes from r and writes them to Kafka in a single message. Implements io.ReaderFrom.
-func (qw *QueuingWriter) ReadFrom(r io.Reader) (n int64, err error) {
+func (qw *QueuingWriter) ReadFrom(r io.Reader) (int64, error) {
 	p, err := ioutil.ReadAll(r)
 	if err != nil {
 		return 0, err
@@ -152,12 +95,6 @@ func (qw *QueuingWriter) Closed() bool {
 // SetLogger sets the logger used by this QueuingWriter.
 func (qw *QueuingWriter) SetLogger(l *log.Logger) {
 	qw.log = l
-}
-
-func (qw *QueuingWriter) stopListener() {
-	atomic.StoreInt32(&qw.closed, 1)
-	qw.pendingWg.Wait() // wait for pending writes to complete
-	close(qw.stopCh)    // signal response listener stop
 }
 
 // CloseAll closes both the underlying Producer and Client. If the writer has already been closed, CloseAll will
@@ -181,7 +118,8 @@ func (qw *QueuingWriter) Close() (err error) {
 		return syscall.EINVAL
 	}
 
-	qw.stopListener()
+	atomic.StoreInt32(&qw.closed, 1)
+	qw.pendingWg.Wait() // wait for pending writes to complete
 
 	var me *MultiError
 
